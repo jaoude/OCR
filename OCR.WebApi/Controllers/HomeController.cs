72766b7b9ee33c110346b07d19abb607789bf519
Peter <peter_bachour@icloud.com>
2019-01-16 16:15:10 +0200

    wip

--- OCR.WebApi/Controllers/HomeController.cs
+++ OCR.WebApi/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using OCR.WebApi.Models;
 
@@ -21,7 +22,7 @@ namespace OCR.WebApi.Controllers
 
             return View();
         }
-
+        [Authorize(Roles = "admin")]
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
