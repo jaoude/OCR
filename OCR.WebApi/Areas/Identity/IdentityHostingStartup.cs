4e438ff35a0e992add482cbcd0dfa2a400c7b97a
Peter <peter_bachour@icloud.com>
2019-01-16 15:38:45 +0200

    wip

--- OCR.WebApi/Areas/Identity/IdentityHostingStartup.cs
+++ OCR.WebApi/Areas/Identity/IdentityHostingStartup.cs
@@ -14,7 +14,8 @@ namespace OCR.WebApi.Areas.Identity
     {
         public void Configure(IWebHostBuilder builder)
         {
-            builder.ConfigureServices((context, services) => {
+            builder.ConfigureServices((context, services) =>
+            {
             });
         }
     }
